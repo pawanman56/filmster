c53cac668485576759dd109711367d5a3617c919
Pawan Sunuwar <pawanman56@gmail.com>
2017-03-01 10:39:02 +0000

    Build Movie show pages

--- app/services/movie_builder.rb
+++ app/services/movie_builder.rb
@@ -7,26 +7,26 @@ class MovieBuilder
     def build!
         return @movie unless @movie.new_record?
         
-            @fetched_movie = Omdb::Api.find_by(id: @imdbid)
-            
-            @movie.title = @fetched_movie.title
-            @movie.year = @fetched_movie.year
-            @movie.rated = @fetched_movie.rated
-            @movie.released = @fetched_movie.released
-            @movie.runtime = @fetched_movie.runtime
-            @movie.genre = @fetched_movie.genre
-            @movie.director = @fetched_movie.director
-            @movie.writer = @fetched_movie.writer
-            @movie.actors = @fetched_movie.actors
-            @movie.plot = @fetched_movie.plot
-            @movie.language = @fetched_movie.language
-            @movie.country = @fetched_movie.country
-            @movie.awards = @fetched_movie.awards
-            @movie.poster = @fetched_movie.poster
-            @movie.metascore = @fetched_movie.metascore
-            @movie.imdbrating = @fetched_movie.imdbrating
-            @movie.imdbvotes = @fetched_movie.imdbvotes
-            @movie.imdbid = @fetched_movie.imdbid
+        @fetched_movie = Omdb::Api.find_by(id: @imdbid)
+        
+        @movie.title = @fetched_movie.title
+        @movie.year = @fetched_movie.year
+        @movie.rated = @fetched_movie.rated
+        @movie.released = @fetched_movie.released
+        @movie.runtime = @fetched_movie.runtime
+        @movie.genre = @fetched_movie.genre
+        @movie.director = @fetched_movie.director
+        @movie.writer = @fetched_movie.writer
+        @movie.actors = @fetched_movie.actors
+        @movie.plot = @fetched_movie.plot
+        @movie.language = @fetched_movie.language
+        @movie.country = @fetched_movie.country
+        @movie.awards = @fetched_movie.awards
+        @movie.poster = @fetched_movie.poster
+        @movie.metascore = @fetched_movie.metascore
+        @movie.imdbrating = @fetched_movie.imdbrating
+        @movie.imdbvotes = @fetched_movie.imdbvotes
+        @movie.imdbid = @fetched_movie.imdbid
         
         @movie.save!
         
